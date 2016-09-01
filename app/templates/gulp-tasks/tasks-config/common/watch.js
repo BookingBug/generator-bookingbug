8fa9f8580f9cf437e76617d2b751d0b776bd8e17
damian pudlo <pudlo.damian@gmail.com>
2016-09-01 21:10:02 +0100

    watch task performance improved by using more specific globs

--- app/templates/gulp-tasks/tasks-config/common/watch.js
+++ app/templates/gulp-tasks/tasks-config/common/watch.js
@@ -53,10 +53,10 @@
             var projectFiles = [
                 configuration.projectRootPath + '/src/javascripts/**/*.js',
                 configuration.projectRootPath + '/src/javascripts/**/*.js.coffee',
-                '!**/*.spec.js',
-                '!**/*.spec.js.coffee',
-                '!**/*.js.js',
-                '!**/*.js.map'
+                '!' + configuration.projectRootPath + '/src/javascripts/**/*.spec.js',
+                '!' + configuration.projectRootPath + '/src/javascripts/**/*.spec.js.coffee',
+                '!' + configuration.projectRootPath + '/src/javascripts/**/*.js.js',
+                '!' + configuration.projectRootPath + '/src/javascripts/**/*.js.map'
             ];
 
             gulp.watch(projectFiles, ['scripts:client']);
