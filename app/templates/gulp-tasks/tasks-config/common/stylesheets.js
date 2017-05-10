8096c323bac5595bf006c2b1f5dd0622c834d5c7
ZOODUCK (Wesley Pumpkinhead) <zooduck@BeNiceOrEatSomeRice.com>
2017-05-10 12:39:38 +0100

    Add gulp-bless plugin to automatically generate multiple css files when IE9 selector limit is reached. Include --ignoreBless flag to override.

--- app/templates/gulp-tasks/tasks-config/common/stylesheets.js
+++ app/templates/gulp-tasks/tasks-config/common/stylesheets.js
@@ -1,6 +1,7 @@
 (function () {
     'use strict';
 
+    const argv = require('yargs').argv;
     const gulpConcat = require('gulp-concat');
     const gulpCssSelectorLimit = require('gulp-css-selector-limit');
     const gulpPlumber = require('gulp-plumber');
@@ -8,6 +9,9 @@
     const gulpSourcemaps = require('gulp-sourcemaps');
     const mainBowerFiles = require('main-bower-files');
     const path = require('path');
+    const gulpIf = require('gulp-if');
+    const guipBless = require('gulp-bless');
+    const gulpUtil = require('gulp-util');
 
     module.exports = function (gulp, configuration) {
 
@@ -18,6 +22,12 @@
             return (
                 path.match(/\.css$/) && (path.indexOf('bootstrap.') === -1) && (path.indexOf('bookingbug-angular-') === -1)
             );
+        }
+
+        function blessErrorHandler (err) {
+            var msg = "gulp-bless plugin has failed to validate booking-widget.css. Use --ignoreBless flag or update your project to use SDK v1.4.26+ or v2.2.17+";
+            gulpUtil.log(gulpUtil.colors.bgRed(msg));
+            process.exit(1);
         }
 
         function stylesheetsVendorsTask() {
@@ -33,6 +43,7 @@
 
             gulp.src(dependenciesCssFiles)
                 .pipe(gulpConcat('booking-widget-dependencies.css'))
+                .pipe(gulpIf(!argv.ignoreBless, gulpBless({log: false}))).on('error', blessErrorHandler)
                 .pipe(gulp.dest(configuration.projectReleasePath));
         }
 
@@ -54,6 +65,7 @@
                 .pipe(gulpPlumber())
                 .pipe(gulpSass(gulpSassOptions).on('error', gulpSass.logError))
                 .pipe(gulpConcat('booking-widget.css'))
+                .pipe(gulpIf(!argv.ignoreBless, gulpBless({log: false}))).on('error', blessErrorHandler)
                 .pipe(gulpCssSelectorLimit.reporter('fail'))
                 .pipe(gulpSourcemaps.write('maps', {includeContent: false}))
                 .pipe(gulp.dest(configuration.projectReleasePath));
